0fb709b5c5bf952686eabbed0b6f8d609b6e2f68
Jack Wright <jack.wright@jivesoftware.com>
2013-05-07 17:38:33 -0700

    initial

---
+++ src/main/scala/com/jivesoftware/gamification/GamificationServlet.scala
@@ -0,0 +1,17 @@
+package com.jivesoftware.gamification
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class GamificationServlet extends GamificationserverStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
